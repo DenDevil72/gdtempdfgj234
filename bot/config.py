74c933638d589b6cea0853bebdcae4f8d2e7245b
DenDevil72 <74084635+DenDevil72@users.noreply.github.com>
2021-04-07 09:12:57 +0530

    Update config.py

--- bot/config.py
+++ bot/config.py
@@ -1,9 +1,9 @@
 class config:
-    BOT_TOKEN = ""
-    APP_ID = ""
-    API_HASH = ""
-    DATABASE_URL = ""
-    SUDO_USERS = "" # Sepearted by space.
+    BOT_TOKEN = "1734355387:AAHGQThxbmWjund0xhgHPzwszKCwCG8pgzk"
+    APP_ID = "2198783"
+    API_HASH = "5fcc46ed98ba8152e883ed2e3f217417"
+    DATABASE_URL = "postgres://rwwhuqihjkmewb:aa75a61a468be9ac47e27124d2be0e5eb9ee2d336681ca87922f94db46371c86@ec2-54-162-119-125.compute-1.amazonaws.com:5432/dc07daugmrcu4o"
+    SUDO_USERS = "1149041218" # Sepearted by space.
     SUPPORT_CHAT_LINK = ""
     DOWNLOAD_DIRECTORY = "./downloads/"
     G_DRIVE_CLIENT_ID = ""
